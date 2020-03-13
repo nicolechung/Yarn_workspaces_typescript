87c5886e1aeae4bc3875c4b2e09886a45e2ccde7
Nicole Chung <nicole.chung@gmail.com>
2020-03-13 17:05:06 -0400

    now working in docs

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { Button } from "@fakescope/components";
+
 export default function Index() {
   return (
     <div>
       <p>Hello Next.js</p>
+      <Button>A button from components</Button>
     </div>
   );
 }
